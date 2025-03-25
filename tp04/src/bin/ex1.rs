a01c694bca202634c5c2639f69dd1676d8a7145c
endy090 <lamarielhadi1@gmail.com>
2025-03-25 12:32:10 +0100

    Update ex1.rs
    Créer un programme de sorte que le contenu d'un fichier qui est reçu comme premier argument soit redirigée vers un fichier reçu en tant que deuxième argument - une fonctionnalité similaire à l'utilitaire cp de Linux.

--- tp04/src/bin/ex1.rs
+++ tp04/src/bin/ex1.rs
@@ -1,3 +1,32 @@
 use std::io;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
-fn main() {}
+fn main() {
+ 
+    let number = Box::new(10);
+    println!("Valeur dans la Box : {}", number);
+
+  
+    let counter = Arc::new(Mutex::new(0));
+
+    let mut handles = vec![];
+
+    for _ in 0..5 {
+        let counter_clone = Arc::clone(&counter);
+        
+        let handle = thread::spawn(move || {
+            let mut num = counter_clone.lock().unwrap();
+            *num += 1;
+            println!("Compteur mis à jour par un thread : {}", num);
+        });
+
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Valeur finale du compteur : {}", *counter.lock().unwrap());
+}
